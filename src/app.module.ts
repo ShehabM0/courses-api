5a9f4bb6740053170fa486ca433718aa39dead13
ShehabM0 <shehabmohamed2357@gmail.com>
2026-04-08 22:52:42 +0200

    add user entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,23 @@
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
-import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppService } from './app.service';
+import { User } from './users/user.entity';
 import { Module } from '@nestjs/common';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true, 
+    ConfigModule.forRoot({ isGlobal: true }),
+
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        url: config.get<string>('DATABASE_URL'),
+        ssl: true,
+        entities: [User],
+        synchronize: process.env.NODE_ENV === 'DEV',
+      }),
     }),
   ],
   controllers: [AppController],
---
+++ src/users/user.entity.ts
@@ -0,0 +1,40 @@
+// strictPropertyInitialization -> ! 
+
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+
+export enum UserRole {
+  STUDENT = 'student',
+  INSTRUCTOR = 'instructor',
+  ADMIN = 'admin',
+}
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id!: string;
+
+  @Column({ length: 100 })
+  name!: string;
+
+  @Column({ unique: true })
+  email!: string;
+
+  @Column()
+  password!: string;
+
+  @Column({
+    type: 'enum',
+    enum: UserRole,
+    default: UserRole.STUDENT,
+  })
+  role!: UserRole;
+
+  @Column({ default: false })
+  isVerified!: boolean;
+
+  @CreateDateColumn()
+  createdAt!: Date;
+
+  @UpdateDateColumn()
+  updatedAt!: Date;
+}
