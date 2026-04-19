9ea2042ccbb2fe536397883906d5e8dca913ad17
ShehabM0 <shehabmohamed2357@gmail.com>
2026-04-19 14:07:52 +0200

    add MailService with SMTP configuration

---
+++ src/mail/mail.module.ts
@@ -0,0 +1,32 @@
+// mail.module.ts
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { MailService } from './mail.service';
+import { Module } from '@nestjs/common';
+
+@Module({
+  imports: [
+    ConfigModule,
+    MailerModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        transport: {
+          host: config.get<string>('SMTP_HOST'),
+          port: config.get<number>('SMTP_PORT'),
+          secure: false,
+          auth: {
+            user: config.get<string>('SMTP_USER'),
+            pass: config.get<string>('SMTP_PASS'),
+          },
+        },
+        defaults: {
+          from: '"EduC" <no-reply@example.com>',
+        },
+      }),
+    }),
+  ],
+  providers: [MailService],
+  exports: [MailService],
+})
+export class MailModule {}
